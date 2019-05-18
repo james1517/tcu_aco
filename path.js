f1ac16635f1fd28bd6e5660401da1a203dcfe86e
james1517 <44334362+james1517@users.noreply.github.com>
2019-05-18 15:47:20 +0900

    Add files via upload

--- path.js
+++ path.js
@@ -1,13 +1,14 @@
-const NODE_NUMBER = 4; //ノードの数//一つの軸で
+const NODE_NUMBER = 2; //ノードの数
+const AXIS = 4;//軸の数
 const START_NODE_X = 50;//アリの巣のx座標
-const START_NODE_Y = 50;//アリの巣のy座標
+const START_NODE_Y = Math.floor(Math.random() * 1000);//アリの巣のy座標
 const RANDOM_MIN_Y = 1;//y座標のランダム下限範囲
-const RANDOM_MAX_Y = 300;//y座標のランダム上限範囲
-const GOAL_Y = 300;//エサのy座標
+const RANDOM_MAX_Y = 250;//y座標のランダム上限範囲
+const GOAL_Y = Math.floor(Math.random() * 1000);;//エサのy座標
 
 //座標の決定
-route_y = new Array(NODE_NUMBER);
-for (let y = 0; y < NODE_NUMBER; y++) {
+route_y = new Array(AXIS);
+for (let y = 0; y < AXIS; y++) {
     route_y[y] = new Array(NODE_NUMBER).fill(0);
 
     //ランダムで座標の決定
@@ -15,7 +16,7 @@ for (let y = 0; y < NODE_NUMBER; y++) {
         if(z == 0){
             route_y[y][z] = Math.floor(Math.random() * (RANDOM_MAX_Y + 1 - RANDOM_MIN_Y)) + RANDOM_MIN_Y;
         }else{
-            route_y[y][z] = Math.floor(Math.random() * (RANDOM_MAX_Y + 1 - RANDOM_MIN_Y)) + route_y[y][z - 1];
+            route_y[y][z] = Math.floor(Math.random() * (RANDOM_MAX_Y + 1 - RANDOM_MIN_Y)) + route_y[y][z - 1] + 50;
         }
         //座標デバッグ
         console.log("軸："+y+"　,上から"+z);
@@ -44,11 +45,12 @@ let random_point = 0;//ランダムの判断値
 
 //軸は3つ
 //経路の描画
-for(let y = 0; y < NODE_NUMBER - 1; y++){//軸の分だけ回す
+for(let y = 0; y < AXIS - 1; y++){//軸の分だけ回す
     random_point = Math.floor(Math.random() * NODE_NUMBER);
     for(let z = 0; z < NODE_NUMBER; z++){//座標の分だけ回す        
         drawPath(bifurcation, route_y[y][z], bifurcation + 200, route_y[y + 1][z]);
         
+        //分岐の生成
         if(random_point == z){
             if(random_point == 0){
                drawPath(bifurcation, route_y[y][z], bifurcation + 200, route_y[y + 1][z + 1]);
@@ -62,6 +64,6 @@ for(let y = 0; y < NODE_NUMBER - 1; y++){//軸の分だけ回す
 
 //経路からゴールへの描画
 for(let z = 0; z < NODE_NUMBER; z++){
-    drawPath(bifurcation, route_y[NODE_NUMBER - 1][z], bifurcation + 200, GOAL_Y);
+    drawPath(bifurcation, route_y[AXIS - 1][z], bifurcation + 200, GOAL_Y);
 }
 
