b8ce02ba6146df71490c4c891ee4841cee0dd3f9
Ryomm <50630425+Ryomm@users.noreply.github.com>
2019-05-29 23:55:42 +0900

    背景をつけました

--- aco/visual.js
+++ aco/visual.js
@@ -1,7 +1,18 @@
+//
+// var canvas=document.querySelector('#bkimg');
+// var context = canvas.getContext('2d');
+// img=new Image();
+// img.onload=function(){
+//   context.drawImage(bgimg_sample.jpg,0,0);
+// }
 
-var canvas=document.querySelector('#bkimg');
-var context = canvas.getContext('2d');
-img=new Image();
-img.onload=function(){
-  context.drawImage(bgimg_sample.jpg,0,0);
+function draw(){
+  var canvas=document.querySelector('#canvas');
+  var context = canvas.getContext('2d');
+
+  var img =new Image();
+  img.src="bkimg.jpg";
+  img.onload = function(){
+    context.drawImage(img, 0, 0, 1000, 500);
+  }
 }
