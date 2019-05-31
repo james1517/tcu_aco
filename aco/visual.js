bc97d772e963e1f52d7829e500bcd30d7e4d63af
Ryomm <50630425+Ryomm@users.noreply.github.com>
2019-05-31 13:25:20 +0900

    画像を軽くしました

--- aco/visual.js
+++ aco/visual.js
@@ -1,12 +1,1 @@
 
-
-function draw(){
-  var canvas=document.querySelector('#canvas');
-  var context = canvas.getContext('2d');
-
-  var img =new Image();
-  img.src="bkimg.jpg";
-  img.onload = function(){
-    context.drawImage(img, 0, 0, 1000, 500);
-  }
-}
